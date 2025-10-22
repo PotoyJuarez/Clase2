49e62f520d133546595b9195106c06825563440e
Potoy <potoyjuarez@gmail.com>
2025-10-21 19:50:08 -0600

    feat: se agrega top 6

--- cod/Limpieza_Cars.R
+++ cod/Limpieza_Cars.R
@@ -5,6 +5,8 @@ library(tidyverse)
 cars %>% 
   ggplot(aes(speed, dist)) +
   geom_line(colour = '#00ff00') +
-  geom_smooth() +
   labs(x = 'Velocidad', y = 'Distancia') +
   theme_minimal()
+
+cars %>% 
+  head()
